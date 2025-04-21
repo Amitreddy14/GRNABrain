94c606a4ee5fe22d36779160bfb5ee223798d087
Amit Reddy <jsammureddy140804@gmail.com>
2025-04-21 22:06:20 +0530

    Add one-hot encoding function for nucleotide bases in utils.py

--- utils.py
+++ utils.py
@@ -7,3 +7,12 @@ def debug_print(statements=[], end='\n'):
     for statement in statements:
         print(statement, end=' ')
     print(end=end)
+
+def ohe_base(base):
+    base = base.lower()
+    ohe = np.zeros((1, 4))
+    if base == 'a': ohe[0, 0] = 1
+    if base == 'g': ohe[0, 1] = 1
+    if base == 'c': ohe[0, 2] = 1
+    if base == 't': ohe[0, 3] = 1
+    return ohe    
