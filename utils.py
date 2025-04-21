7a8bae0c831361ce95147352042dc63dbf25dc40
Amit Reddy <jsammureddy140804@gmail.com>
2025-04-21 22:07:07 +0530

    end of file and ensure proper formatting in ohe_base and ohe_bases functions

--- utils.py
+++ utils.py
@@ -16,3 +16,12 @@ def ohe_base(base):
     if base == 'c': ohe[0, 2] = 1
     if base == 't': ohe[0, 3] = 1
     return ohe    
+
+def ohe_bases(bases_lists):
+    debug_print(['one-hot encoding bases'])
+    ohe = np.zeros((len(bases_lists), len(bases_lists[0]), 4))
+    for i, bases_list in enumerate(bases_lists):
+        for j, base in enumerate(bases_list):
+            if j >= len(bases_lists[0]): continue
+            ohe[i, j] = ohe_base(base)
+    return ohe
