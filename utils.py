5a4ea9ec3db7a790ce79510b95caa7e630421f96
Amit Reddy <jsammureddy140804@gmail.com>
2025-04-21 22:03:40 +0530

    include debug_print function and ensure proper newline at end of file

--- utils.py
+++ utils.py
@@ -1,2 +1,9 @@
 import datetime
 import numpy as np
+
+def debug_print(statements=[], end='\n'):
+    ct = datetime.datetime.now()
+    print('[', str(ct)[:19], '] ', sep='', end='')
+    for statement in statements:
+        print(statement, end=' ')
+    print(end=end)
