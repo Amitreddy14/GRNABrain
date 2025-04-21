8cb00d84231e990a38675e883c7c725bb07b7776
Amit Reddy <jsammureddy140804@gmail.com>
2025-04-21 21:56:20 +0530

    Add genome sequence loading function to preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -13,3 +13,9 @@ from utils import *
 
 REFERENCE_GENOME_PATH = 'data/GCF_000001405.26_GRCh38_genomic.fna' # https://www.ncbi.nlm.nih.gov/datasets/genome/GCF_000001405.26/
 GRNA_PATH = 'data/hg_guide_info.csv'  # https://www.ncbi.nlm.nih.gov/genome/guide/human/
+
+GENOME_SEQUENCES = {}
+def read_genome(path=REFERENCE_GENOME_PATH):
+    debug_print(['loading genomic data from', path])
+    for record in SeqIO.parse(path, 'fasta'):
+        GENOME_SEQUENCES[record.id.split('.')[0]] = record.seq
