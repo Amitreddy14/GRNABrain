ced6e725dca24e3c8d54caa2eb85a5f675d0a077
Amit Reddy <jsammureddy140804@gmail.com>
2025-04-21 21:54:51 +0530

    reference genome and GRNA path constants to preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -10,3 +10,6 @@ from Bio import SeqIO
 import pyBigWig
 
 from utils import *
+
+REFERENCE_GENOME_PATH = 'data/GCF_000001405.26_GRCh38_genomic.fna' # https://www.ncbi.nlm.nih.gov/datasets/genome/GCF_000001405.26/
+GRNA_PATH = 'data/hg_guide_info.csv'  # https://www.ncbi.nlm.nih.gov/genome/guide/human/
