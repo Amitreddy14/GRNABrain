5564d26c65057b7158a79dc36d15282bc14b6a01
Amit Reddy <jsammureddy140804@gmail.com>
2025-04-22 23:38:38 +0530

    Fix import statement for matplotlib in perturbation.py

--- perturbation.py
+++ perturbation.py
@@ -1,2 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+def perturbation_analysis(gan, rnas, chromosomes, starts, ends, base, a=400, view_length=23, num_seqs=None):
+    if not num_seqs: num_seqs = len(rnas)
+    
+    skipped = 0
+    skip = []
