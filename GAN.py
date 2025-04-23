9e7895aa42b233558399067ad1aec08ac5eeb825
Amit Reddy <jsammureddy140804@gmail.com>
2025-04-24 00:06:52 +0530

    Implement discriminator loss function for GAN training

--- GAN.py
+++ GAN.py
@@ -10,3 +10,16 @@ from tqdm import tqdm
 import preprocessing
 from preprocessing import debug_print
 from models import *
+
+def discriminator_loss(real_output, pred_output, mismatch_output):
+    lambda1 = 0.4
+    lambda2 = 0.2
+    lambda3 = 0.4
+    # BC(1, D(g_t, d_t))
+    real_loss = tf.keras.losses.BinaryCrossentropy()(tf.ones_like(real_output), real_output)
+    # BC(0, D(G(d_t), d_t))
+    fake_loss = tf.keras.losses.BinaryCrossentropy()(tf.zeros_like(pred_output), pred_output)
+    # BC(0, D(g_t, d_rand))
+    mismatch_loss = tf.keras.losses.BinaryCrossentropy()(tf.zeros_like(mismatch_output), mismatch_output)
+    total_loss = real_loss * lambda1 + fake_loss * lambda2 + mismatch_loss * lambda3
+    return total_loss
