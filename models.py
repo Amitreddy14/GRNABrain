9322e9f069b178321b10c54303789ba083557c5c
Amit Reddy <jsammureddy140804@gmail.com>
2025-04-23 16:11:10 +0530

    Transformer layer implementation with initialization parameters

--- models.py
+++ models.py
@@ -2,3 +2,12 @@ import numpy as np
 import tensorflow as tf
 
 from utils import *
+
+# Layers
+class Transformer(tf.keras.layers.Layer):
+    def __init__(self, num_heads, head_size, ff_dim, dropout=0.1, **kwargs):
+        super(Transformer, self).__init__(**kwargs)
+        self.num_heads = num_heads
+        self.head_size = head_size
+        self.ff_dim = ff_dim
+        self.dropout_rate = dropout
