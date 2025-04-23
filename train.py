8f9f65df3e26ab5e253d1bfd9eb868879a17df4e
Amit Reddy <jsammureddy140804@gmail.com>
2025-04-24 00:53:52 +0530

    compute_baselines function to evaluate model performance

--- train.py
+++ train.py
@@ -8,3 +8,9 @@ from preprocessing import debug_print
 from models import *
 from GAN import *
 from analysis import *
+
+def compute_baselines(models, X, Y):
+    for model in models:
+        baseline_pred = model.predict(X)
+        baseline_loss = tf.math.reduce_mean(tf.keras.losses.categorical_crossentropy(Y, baseline_pred))
+        debug_print([model.name, 'loss:', baseline_loss.numpy()])
