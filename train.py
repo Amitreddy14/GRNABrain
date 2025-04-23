932990183ac790f6e950ed2f7ae87895019f5840
Amit Reddy <jsammureddy140804@gmail.com>
2025-04-24 00:54:30 +0530

    refactor: enhance compute_baselines and train functions for improved model training

--- train.py
+++ train.py
@@ -14,3 +14,12 @@ def compute_baselines(models, X, Y):
         baseline_pred = model.predict(X)
         baseline_loss = tf.math.reduce_mean(tf.keras.losses.categorical_crossentropy(Y, baseline_pred))
         debug_print([model.name, 'loss:', baseline_loss.numpy()])
+
+def train(models, X, Y, epochs, batch_size=64, validation_split=0.2, graph=True, summary=True, loss='categorical_crossentropy'):
+    debug_print(['training model'])
+
+    for model in models:
+        model.compile(loss=loss, optimizer=tf.keras.optimizers.legacy.Adam(), metrics=['accuracy'])
+        model(X)
+        if summary: model.summary()
+        model.fit(X, Y, batch_size=batch_size, epochs=epochs, validation_split=validation_split)        
