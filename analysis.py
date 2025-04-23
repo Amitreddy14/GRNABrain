b87551993ad49f77e951b97ab66ef088e42fb646
Amit Reddy <jsammureddy140804@gmail.com>
2025-04-24 02:12:40 +0530

    feat: add perturbation_analysis function for genomic sequence and epigenomic signal processing

--- analysis.py
+++ analysis.py
@@ -197,3 +197,49 @@ def candidate_grna_range(gan, chromosome, start, end, a=400, view_length=23, num
         plt.show()
     
     return best_seqs
+
+def perturbation_analysis(gan, rnas, chromosomes, starts, ends, base, a=400, view_length=23, num_seqs=None, plot=True):
+    if not num_seqs: num_seqs = len(rnas)
+    
+    skipped = 0
+    skip = []
+
+    X_gen = np.zeros((len(rnas), view_length, 8))
+    X = np.zeros((len(rnas), view_length + 2 * a, 8))
+    for n in range(num_seqs):
+        try:
+            seq = preprocessing.fetch_genomic_sequence(chromosomes[n], starts[n] - a, ends[n] + a).lower()
+            ohe_seq = np.concatenate([preprocessing.ohe_base(base) for base in seq], axis=0)
+            epigenomic_signals = preprocessing.fetch_epigenomic_signals(chromosomes[n], starts[n] - a, ends[n] + a)
+            epigenomic_seq = np.concatenate([ohe_seq, epigenomic_signals], axis=1)
+            if np.isnan(epigenomic_seq).any():
+                skip.append(n)
+                continue
+            
+            X[n] = epigenomic_seq
+            X_gen[n] = epigenomic_seq[a:a + view_length]
+        except:
+            skip.append(n)
+            continue
+
+    pred_Yi = gan.generator(X_gen)
+    pred = np.argmax(pred_Yi, axis=2)
+    real = np.argmax(rnas, axis=2)
+    real_Yi = gan.get_real_Yi(pred_Yi, pred, real)
+    axis_index = 0
+    cumulative_percent_diff = []
+
+    original = activity_test(
+        gan=gan,
+        rnas=rnas,
+        chromosomes=chromosomes,
+        starts=starts,
+        ends=ends,
+        a=a,
+        view_length=view_length,
+        plot=False,
+        num_seqs=num_seqs,
+        return_sep=True
+        )
+    
+    heatmap = np.zeros((len(rnas), a * 2 - view_length))
